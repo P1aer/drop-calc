d4f63c54c2bbeeedf3d81e81bab9c3a518f80cf3
iosipov <iosipov@emphasoft.com>
2023-03-12 01:09:39 +0300

    double click func complete

--- src/components/DropZoneBlock/BuildingBlockZone.tsx
+++ src/components/DropZoneBlock/BuildingBlockZone.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { DragItem } from '../../redux/slices/DragDropSlice'
+import { DragItem, excludeFromZone } from '../../redux/slices/DragDropSlice'
 import Stack from 'react-bootstrap/Stack'
+import { useAppDispatch } from '../../redux/hooks'
 interface BuildingBlockZoneProps {
   blocks: DragItem[]
 }
 const BuildingBlockZone:React.FC<BuildingBlockZoneProps> = ({blocks}) => {
+  const dispatch = useAppDispatch()
+  const onDoubleClick = (block: DragItem) => () => {
+    dispatch(excludeFromZone(block))
+  }
   return (
     <Stack gap={3} className='align-self-end'>
       {blocks.map((elem) => (
         <div
+          onDoubleClick={onDoubleClick(elem)}
           className='component-base'
           key={elem.block.id}>{elem.block.block(undefined)}
         </div>
